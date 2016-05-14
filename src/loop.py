7139d70ac3ad791acfcd6b1f11585917ef2ce414
nixorn <a-kanev@yandex.ru>
2016-05-14 21:50:25 +0300

    start zaloooping

---
+++ src/loop.py
@@ -0,0 +1,3 @@
+from .vk_support import api
+
+api.users.get(user_ids=21731448, fields=['last_seen'])
--- src/vk_support.py
+++ src/vk_support.py
@@ -3,6 +3,6 @@ import vk
 
 session = vk.AuthSession(app_id=app_id, user_login=login, user_password=password)
 api = vk.API(session)
-api.users.get(user_ids=21731448, fields=['last_seen'])
+
 
 
