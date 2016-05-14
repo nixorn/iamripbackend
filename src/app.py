89bd640786b4a2c7a2df11719a102b8a120058cc
nixorn <a-kanev@yandex.ru>
2016-05-14 15:41:02 +0300

    Templates support

--- src/app.py
+++ src/app.py
@@ -4,10 +4,12 @@ from uuid import uuid4
 from .models import *
 from .engine import *
 from .api import api_bp
+from .views import views_bp
 
 
 app = Flask(__name__)
-app.register_blueprint(api_bp, url_prefix='/api')    
+app.register_blueprint(api_bp, url_prefix='/api')
+app.register_blueprint(views_bp)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
---
+++ src/views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, abort
+
+views_bp = Blueprint('views_bp', __name__, template_folder='templates')
+@views_bp.route('/registration')
+def registration():
+    try:
+        return render_template('registration.jade')
+    except TemplateNotFound:
+        abort(404)
