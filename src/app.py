ab79d77b9890c22a0b5b10e2816b3677545e9e52
Sergey Ukolov <zezic51@yandex.ru>
2016-05-14 16:14:36 +0300

    Move imports earlier

--- src/app.py
+++ src/app.py
@@ -8,10 +8,10 @@ from .views import views_bp
 
 
 app = Flask(__name__)
-app.register_blueprint(api_bp, url_prefix='/api')
-app.register_blueprint(views_bp)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+app.register_blueprint(api_bp, url_prefix='/api')
+app.register_blueprint(views_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
