8797b83715eed79a987e0f2fd914311b4a2096b8
nixorn <a-kanev@yandex.ru>
2016-05-14 21:42:30 +0300

    new try to use vk api

--- src/vk_support.py
+++ src/vk_support.py
@@ -1,5 +1,8 @@
-from config import token
+from config import login, password, app_id
 import vk
 
-session = vk.Session(access_token=token)
+session = vk.AuthSession(app_id=app_id, user_login=login, user_password=password)
 api = vk.API(session)
+api.users.get(user_ids=21731448, fields=['last_seen'])
+
+
