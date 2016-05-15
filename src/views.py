082a9cfa529323f429b62281054e7e35bd256d2c
Sergey Ukolov <zezic51@yandex.ru>
2016-05-15 18:52:59 +0300

    Help

--- src/views.py
+++ src/views.py
@@ -58,7 +58,7 @@ def help():
     logged_in = False
     if is_user_logged_it(request):
         logged_in = True
-    return render_template('help.jade', logged_in=logged_in, is_profile=False, user=user)
+    return render_template('help.jade', logged_in=logged_in, is_profile=False)
 
 @views_bp.route('/profile')
 @views_bp.route('/profile/settings')
